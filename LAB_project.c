d5b4c361cce209790e1a96fe9c3d516780cb57a9
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-03-19 14:27:24 +0600

    Update LAB_project.c

--- LAB_project.c
+++ LAB_project.c
@@ -2,25 +2,98 @@
 int main ()
 //Calculation
 {
-    for (r=0; r<45; r++)             //increased row
+    int row,col;
+    float ara[45][8],gpa;
+
+     //all intial value 0;
+    for (row = 0; row<45; row++)
     {
-        gpa =0;                            //initial gpa 0;
-        for (c=1; c<7; c++)         // increased column
+        for(col=0; col<8; col++)
         {
-            if (c==1 || c== 2 || c== 4)            //only have  3 or 1 credit;count for 3 credit
+            ara[row][col]=0;
+        }
+    }
+
+
+    // scanned for first row,delete when integrated;
+    for (row=0,col=0; col<7; col++)
+    {
+        scanf (" %f", &ara[row][col]);
+    }
+
+
+    // replaced number with grade for each subject
+    for (row=0; row<45; row++)
+    {
+        for (col=1; col<7; col++)
+        {
+            if (ara[row][col]>=80)
             {
-                gpa = gpa + (3*ar[r][c])/12;
+                ara[row][col]= 4.00;
             }
-            else                                              //count for 1 credit;
+            else if (ara[row][col]>=75)
             {
-                gpa = gpa + (1*ar[r][c])/12;
+                ara[row][col] = 3.75;
+            }
+            else if (ara[row][col]>=70)
+            {
+                ara[row][col] = 3.50;
+            }
+            else if (ara[row][col]>=65)
+            {
+                ara[row][col] = 3.25;
+            }
+            else if (ara[row][col]>=60)
+            {
+                ara[row][col] = 3.00;
+            }
+            else if (ara[row][col]>=55)
+            {
+                ara[row][col] = 2.75;
+            }
+            else if (ara[row][col]>=50)
+            {
+                ara[row][col] = 2.50;
+            }
+            else if (ara[row][col]>=45)
+            {
+                ara[row][col] = 2.25;
+            }
+            else if (ara[row][col]>=40)
+            {
+                ara[row][col] = 2.00;
+            }
+            else
+            {
+                ara[row][col]=0.0;
             }
         }
-        ar[r][7] = gpa;                                   // assigned grade for grade column or column 7
     }
-}
+
+
+    //calculation and assigning grade to col 7;
+    for (row=0; row<45; row++)                                       //increased row
+    {
+        gpa =0;                                                                    //initial gpa 0;
+        for (col=1; col<7; col++)                                        // increased column
+        {
+            if (col==1 || col== 2 || col== 4)                         //only have  3 or 1 credit;count for 3 credit
+            {
+                gpa = gpa + ((3.0*ara[row][col])/12.00);
+            }
+            else                                                                    //count for 1 credit;
+            {
+                gpa = gpa +((1.0*ara[row][col])/12.00);
+            }
+        }
+        ara[row][7] = gpa;                                                 // assigned grade for grade column or column 7
+
+    }
+
 
 // showing id + grade
-for (r=0;r<45;r++){
-    printf ("%d.Student ID: %d           Student Grade:%d\n",(r+1),ar[r][0],ar[r][7]);    //list of Id and grade column;
+    for (row=0; row<45; row++)
+    {
+        printf ("%d.Student ID: %0.0f           Student Grade:%0.2f\n",(row+1),ara[row][0],ara[row][7]);    //list of Id and grade column;
+    }
 }
